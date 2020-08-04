43d78adc9010cf5c0d55f208fd73a1cae414f773
Vinay Agrawal <vinay.agrawal@yantriks.local>
2020-08-04 18:03:29 +0530

    Finale

--- fakenews.py
+++ fakenews.py
@@ -4,4 +4,4 @@ import itertools
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrixpy
